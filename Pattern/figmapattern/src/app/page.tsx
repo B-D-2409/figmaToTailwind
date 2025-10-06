f38fad6d3f6554e4e8cc8e0fbb56638dc6eac113
Masterclass777 <borislav.2404g@gmail.com>
2025-10-06 12:35:11 +0300

    nav to main-page always when we open web-site

--- Pattern/figmapattern/src/app/page.tsx
+++ Pattern/figmapattern/src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/main-page");
+  }, [router]);
+
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center p-4  text-blue-500">
+    <main className="min-h-screen flex flex-col items-center justify-center p-4 text-blue-500">
       <div className="mt-6 w-full max-w-md sm:max-w-lg md:max-w-xl">
       </div>
     </main>
